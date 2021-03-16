d01dfa30e13f8dcda73837f86a911e9ec530ca93
Jared R. Morris <36784267+morrisj95@users.noreply.github.com>
2021-03-16 18:06:47 -0400

    Update 5_ssh_and_git.sh

--- 5_ssh_and_git.sh
+++ 5_ssh_and_git.sh
@@ -10,3 +10,8 @@ echo "Host github.com" >> ~/.ssh/config
 echo " HostName github.com" >> ~/.ssh/config
 echo " IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
 echo "" >> ~/.ssh/config
+
+echo "Host gitlab.com" >> ~/.ssh/config
+echo " HostName gitlab.com" >> ~/.ssh/config
+echo " IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+echo "" >> ~/.ssh/config

19a5717375aa19644b442240cfc9a92de8541913
Jared R. Morris <36784267+morrisj95@users.noreply.github.com>
2021-03-16 23:09:59 -0400

    add kubectl via curl -LO

--- 1_start_here.sh
+++ 1_start_here.sh
@@ -20,7 +20,16 @@ source ~/.bashrc
 sudo apt install python3-virtualenv
 sudo apt install openjdk-8-jre
 sudo apt install node-express-generator
+
+# kubernetes
 sudo apt-get install kubernetes
+mkdir -p ~/tmp/kube
+cd ~/tmp/kube
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
 
 
 # npm
